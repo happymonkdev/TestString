d89adaf388f30f3c7281b9e86d90b6489474bbb5
anand pandey <anand.pandey@kibocommerce.com>
2017-12-07 10:25:47 +0530

    test1

---
+++ TestString/src/com/skill/testjavastring/TestString.java
@@ -0,0 +1,8 @@
+package com.skill.testjavastring;
+
+public class TestString {
+    public static void main(String aar[]) {
+    String s="This is a test string [ hi(ap) test it.]";
+    System.out.println("-->> "+s.split("\\)")[0].split("\\(")[1]);
+    }
+}
