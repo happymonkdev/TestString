f9609e0f4c1e0086cd7235f0ed56b4e0bc74a129
anand pandey <anand.pandey@kibocommerce.com>
2017-12-16 14:32:29 +0530

    commit to local chkOutNewOfMstr

--- TestString/src/com/skill/testjavastring/TestString.java
+++ TestString/src/com/skill/testjavastring/TestString.java
@@ -4,5 +4,6 @@ public class TestString {
     public static void main(String aar[]) {
     String s="This is a test string [ hi(ap) test it.]";
     System.out.println("-->> "+s.split("\\)")[0].split("\\(")[1]);
+    // i want to create new pull request for master branch
     }
 }
