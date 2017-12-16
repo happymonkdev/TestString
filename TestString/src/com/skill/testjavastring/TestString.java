eb36b57a119476197437e8d6b6eb944b6289edb1
anand pandey <anand.pandey@kibocommerce.com>
2017-12-16 13:09:18 +0530

    commit and push of local branch "CreateNewBranch"

--- TestString/src/com/skill/testjavastring/TestString.java
+++ TestString/src/com/skill/testjavastring/TestString.java
@@ -4,5 +4,6 @@ public class TestString {
     public static void main(String aar[]) {
     String s="This is a test string [ hi(ap) test it.]";
     System.out.println("-->> "+s.split("\\)")[0].split("\\(")[1]);
+    System.out.println("This code is of new checkout branch");
     }
 }
